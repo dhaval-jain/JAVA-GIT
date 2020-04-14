708b638dca8b3b029fdaa13b7eb70de1ded44f76
yash40 <63653603+yash40@users.noreply.github.com>
2020-04-14 20:45:35 +0530

    updated studentdatabaseapp
    main function

--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,1 +1,31 @@
+//package studentdatabaseapp;
+import java.util.Scanner;
+
+public class StudentDatabaseApp{
+   
+	public static void main(String[] args) {
+	  /*Student stu1 = new Student();
+		stul.enroll();
+		stul.payTuition();
+		Sysyem.out.println(stu1.toString());
+	  */
+		
+	    //ask no of new users to add
+	    Scanner in = new Scanner(System.in);
+        System.out.print("enter no of students");
+        int numOfStudents= in.nextInt();
+		Student[] students = new Student[numOfStudents];
+		
+	    //create n new students
+		for( int n = 0; n < numOfStudents; n++) {
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+			System.out.println(students[n].toString());
+		
+		}
+		System.out.println("Hello World");
+	}
+}
+
 
